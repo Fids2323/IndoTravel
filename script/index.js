400910b7a2715ed64a3e8e14c2de8fbabf0df22e
Dmitry Homskiy <workfids23@gmail.com>
2023-02-01 13:01:31 +0300

    add animate

--- script/index.js
+++ script/index.js
@@ -1,16 +1,12 @@
-import timer from './modules/timer.js';
-import setDeadline from './modules/control.js';
-import './modules/acc.js';
-import './modules/menuControl.js'
+import timer from "./modules/timer.js";
+import setDeadline from "./modules/control.js";
+import "./modules/acc.js";
+import "./modules/menuControl.js";
+import "./modules/fly.js";
 {
-	const date = new Date(Date.UTC(
-		2023,1,24,20,20
-));
+	const date = new Date(Date.UTC(2023, 1, 24, 20, 20));
 	const init = () => {
-		timer(setDeadline(date))
+		timer(setDeadline(date));
 	};
 	window.timerInit = init;
 }
-
-
-
--- script/modules/menuControl.js
+++ script/modules/menuControl.js
@@ -1,19 +1,51 @@
-const menuButton  = document.querySelector('.header__menu-button');
-const menu  = document.querySelector('.header__menu');
-const header = document.querySelector('.header')
+const menuButton = document.querySelector(".header__menu-button");
+const menu = document.querySelector(".header__menu");
+const header = document.querySelector(".header");
+menu.style.cssText = `
+z-index:1;
+`;
+
+//Menu animation
+const openBurgerMenu = () => {
+	let opacity = 0;
+	setTimeout(function changeOpacity() {
+		opacity += 0.1;
+		menu.style.opacity = opacity;
+		if (opacity < 0.9) {
+			setTimeout(changeOpacity, 100);
+		}
+	}, 100);
+};
+
+const closeBurgerMenu = () => {
+	let opacity = 1;
+	setTimeout(function changeOpacity() {
+		opacity -= 0.1;
+		menu.style.opacity = opacity;
+		if (opacity >= 0) {
+			setTimeout(changeOpacity, 100);
+		}
+	}, 100);
+};
 
 // Toggle the "header__menu_active"
-menuButton .addEventListener('click', ()=> {
-	menu.classList.toggle('header__menu_active')
-})
-
-// If the click event is not on the menu button or menu or menu items
-header.addEventListener("click", function(e) {
-	if (!e.target.closest(".header__menu-button") && !e.target.closest(".header__menu")) {
-		menu.classList.remove("header__menu_active");
+menuButton.addEventListener("click", () => {
+	if (menu.classList.contains("header__menu_active")) {
+		requestAnimationFrame(closeBurgerMenu);
+	} else {
+		requestAnimationFrame(openBurgerMenu);
 	}
-	if (e.target.closest('.header__item')) {
-		menu.classList.remove("header__menu_active")
-	}
+	menu.classList.toggle("header__menu_active");
 });
 
+// If the click event is not on the menu button or menu or menu items
+header.addEventListener("click", function (e) {
+	if (!e.target.closest(".header__menu-button") && !e.target.closest(".header__menu")) {
+		requestAnimationFrame(closeBurgerMenu);
+		menu.classList.remove("header__menu_active");
+	}
+	if (e.target.closest(".header__item")) {
+		requestAnimationFrame(closeBurgerMenu);
+		menu.classList.remove("header__menu_active");
+	}
+});
